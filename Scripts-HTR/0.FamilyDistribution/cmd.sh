cbccb4c05e0b0c2841f1b9c419168a9d6cbd9ef6
zhjilin <zhjilin@gmail.com>
2020-06-22 19:43:23 +0200

    README update
    minor revision of the text in README

--- Scripts-HTR/0.FamilyDistribution/cmd.sh
+++ Scripts-HTR/0.FamilyDistribution/cmd.sh
@@ -1,1 +1,2 @@
+#this will count the number of RBPs in each family
 perl stat_fam_v3.pl dataCSV20181010.tsv 
--- Scripts-HTR/5.InformationContent/cmd170517.sh
+++ Scripts-HTR/5.InformationContent/cmd170517.sh
@@ -1,4 +1,5 @@
-#Information content must be calculated from tree parts, due to the SLM model generates frequency table, instead the normal pwm is the count table. 
+#Information content must be calculated from tree parts. 
+#Note: The SLM model generates frequency table, while the normal pwm is the count table. 
 
 perl IC_calculator17Apr29.pl --m f ../dominating17Apr28/slm_replacement 
 perl IC_calculator17Apr29.pl --m d ../dominating17Apr28/dimatrix
@@ -10,12 +11,10 @@ perl  stem_loop_IC-parser.pl --f 0 Bits_d_2017May24.table fordi_slm.length.table
 perl stem_loop_IC-parser.pl --f 1 Bits_f_2017May24.table forpwm_slm.length.table > Bits_f_2017May24.table.labeled
 Rscript --vanilla StemIC_corrPlot.r Bits_f_2017May24.table.labeled Bits_d_2017May24.table.labeled
 
-#the following section is for the STEM and LOOP comparision, also overall landscape
+#the following section is for the STEM and LOOP comparison, also overall landscape
 
 perl stem_loop_IC-parser.pl --f 0 Bits_f_2017May24.table forpwm_slm.length.table  > Bits_f_2017May24.table.SLM_stemloop
 
 
 cat Bits_n_2017May24.table Bits_f_2017May24.table > All_Bits2017May24.table
 Rscript --vanilla draw_overall_perbase.R All_Bits2017May24.table pubavail.bits.table 0.05
-
-#should change to May24
